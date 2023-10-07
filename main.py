04aec639636ebfeca827fa779c1f5a74aa894c5b
EmilTsaturyan <emiltsaturyansc@gmail.com>
2023-10-07 16:26:27 +0400

    v4

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 import random
 import os
 
-def check(num, coords):
+# Function to check if a number is in the list of available coordinates
+def check(num:int, coords:list) -> bool:
+    """
+    Function return True if num in coords
+    False if not
+    """
     return num in coords
 
-def draw_board(board):
+# Function to draw the tic-tac-toe board
+def draw_board(board:list) -> None:
+    """
+    Function draws tic tac toe board
+    """
     os.system('cls')
     for i in range(len(board)):
         if i == 3 or i == 6:
@@ -12,25 +21,37 @@ def draw_board(board):
         print(board[i], end='')
     print()
 
-def update_board(board, num, current_player='O', coords = []):
+# Function to update the board after a move
+def update_board(board:list, num:int, current_player:str, coords:list) -> None:
+    """
+    Function for update tic-tac-toe board 
+    """
     player_marker = '|X|' if current_player == 'X' else '|O|'
     board[num - 1] = player_marker
     coords.remove(num)
 
-def random_choice(coords):
+# Function to make a random choice for the bot's move
+def random_choice(coords:list) -> int:
+    """
+    Function returns random number from coords
+    """
     return random.choice(coords)
 
-def check_for_win(board, moves):
+# Function to check if there is a winner or a draw
+def check_for_win(board:list, moves:int) -> str:
+    """
+    Function checks if there is a winner or a draw
+    If moves is equal to 9 its draw
+    """
     if (board[0] == board[1] == board[2] == '|X|') or (board[3] == board[4] == board[5] == '|X|') or (board[6] == board[7] == board[8] == '|X|') or (board[0] == board[3] == board[6] == '|X|') or (board[1] == board[4] == board[7] == '|X|') or (board[2] == board[5] == board[8] == '|X|') or (board[0] == board[4] == board[8] == '|X|') or (board[6] == board[4] == board[2] == '|X|'):
         return 'X'
     elif (board[0] == board[1] == board[2] == '|O|') or (board[3] == board[4] == board[5] == '|O|') or (board[6] == board[7] == board[8] == '|O|') or (board[0] == board[3] == board[6] == '|O|') or (board[1] == board[4] == board[7] == '|O|') or (board[2] == board[5] == board[8] == '|O|') or (board[0] == board[4] == board[8] == '|O|') or (board[6] == board[4] == board[2] == '|O|'):
         return 'O'
     else:
         if moves == 9:
-            return 'Draw'
-        else:
-            return False
+            return 'Its a draw'
 
+# Main game loop
 def main():
     print(f"{'-' * 20}Tic-Tac-Toe{'-' * 20}\nX or O 👇")
 
@@ -42,6 +63,7 @@ def main():
 
     moves = 0
 
+    # Get user's choice of X or O
     choose = input('->: ').upper()
     if choose == 'X':
         player_turn = True
@@ -55,11 +77,13 @@ def main():
         print(f'Choose the right option x or y not {choose}')
         exit()
 
+    # Initialize available coordinates and board
     coords = [i for i in range(1, 10)]
     board = ['|1|', '|2|', '|3|', '|4|', '|5|', '|6|', '|7|', '|8|', '|9|']
 
     while True:
         if bot_turn:
+            # Bot's move
             if check(random_choice(coords), coords):
                 update_board(board, random_choice(coords), bot_char, coords)
                 print("Bot's move")
@@ -67,12 +91,17 @@ def main():
             else:
                 break
         else:
-            cord_choose = int(input('Your turn: '))
-            if check(cord_choose, coords):
-                update_board(board, cord_choose, player_char, coords)
-                draw_board(board)
-            else:
-                break
+            try:
+                # Player's turn
+                chosen_number = int(input('Your turn: '))
+                if check(chosen_number, coords):
+                    update_board(board, chosen_number, player_char, coords)
+                    draw_board(board)
+                else:
+                    print("Invalid choice. Please select a number between 1 and 9 that hasn't been chosen yet.")
+                    break
+            except ValueError:
+                print('Please input only numbers')
 
         player_turn = not player_turn
         bot_turn = not bot_turn
